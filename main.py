c5ca81aad3ad3ab89657f5713937701be7ada14d
vladsmnv <vladsmnv@gmail.com>
2019-04-01 09:40:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import timeit
 from gui import MapDrawer
 from reports import write_results_to_file
 from ltm import ltm
-from examples import net_1_inhomogeneous, net_2_Yperman, net_3_merge, net_4_Visum_Yperman, net_5_diverge, net_6_mimo_node, net_7_mimo_node
+from net_tests import net_1_inhomogeneous, net_2_Yperman, net_3_merge, net_4_Visum_Yperman, net_5_diverge, net_6_mimo_node, net_7_mimo_node
 
 # TODO: Add unit tests and profilers based on examples (what to compare for correctness?)
 # net, T, t = net_1_inhomogeneous(15.)  # inhomogeneous node example
